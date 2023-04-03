be917b98d409eff2f41a17d22c5f36ba1c5a033d
Mahmoud Mansouri <mahmoud.mansouri@medtech.tn>
2023-04-03 16:31:57 +0000

    first commit

---
+++ app/src/main/java/com/example/myapplication/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val splashTimeout = 3000L // Duration of splash screen in milliseconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTimeout)
+    }
+}
